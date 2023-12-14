068ab1448f8baca90505f6fa233266f4b8cbaab0
G525922 <nada.abdelhady@idemia.com>
2023-12-14 10:18:33 +0200

    update

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // i am in branch 1
+
+        // update branch 1
     }
 }
